4db8c4752cf9681e1f5d244c55b9f08b2c76deb5
DarshGupta16 <darsh.gupta.1602@gmail.com>
2020-11-18 20:33:34 +0530

    Adding getInputValue and onClick example

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// select and print function
+// select and print functions
 let selectDemo = select("#selectDemo");
 print(selectDemo);
 print(selectDemo.innerHTML);
@@ -9,3 +9,11 @@ changeHTML("#selectDemo", "Changed innerHTML");
 // Select All function
 let allH6 = selectAll("h6");
 for (h6 of allH6) h6.innerHTML = "Changed innerHTML";
+
+// getInputValue and onClick functions
+function executeOnClick() {
+  let myInput = getInputValue("#printText");
+  print(myInput);
+}
+
+onClick("#printTextBtn", executeOnClick);
